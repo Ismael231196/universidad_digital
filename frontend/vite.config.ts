edd20e779f1679b4bb7137cc3227b4809c3db31c
duvan <isdusagu23@gmail.com>
2026-04-12 15:31:35 -0500

    Fix vite to generate dist in frontend directory with absolute path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true
   }
 });
